63fa02f2d71908765a5e229b5e2a5904ccda93e1
gettls <gettls@daum.net>
2022-05-13 18:39:30 +0900

    Fix: controller @RequestBody 바인딩 에러 해결

--- capstone/src/main/java/com/timcook/capstone/user/controller/UserController.java
+++ capstone/src/main/java/com/timcook/capstone/user/controller/UserController.java
@@ -73,7 +73,7 @@ public class UserController {
 	}
 	
 	@PostMapping("/{id}/villages")
-	public ResponseEntity<String> registerVillage(@PathVariable Long id, @RequestBody Long villageId){
+	public ResponseEntity<String> registerVillage(@PathVariable Long id, Long villageId){
 		userService.registerVillage(id, villageId);
 		return ResponseEntity.ok("마을 구독이 완료되었습니다.");
 	}
