687d00d4fbcb5727fad387dece781548b8652757
gettls <gettls@daum.net>
2022-05-13 22:08:00 +0900

    Feat: 유저 등록시 주소 등록 삭제

--- capstone/src/main/java/com/timcook/capstone/user/dto/UserCreateRequest.java
+++ capstone/src/main/java/com/timcook/capstone/user/dto/UserCreateRequest.java
@@ -25,13 +25,6 @@ public class UserCreateRequest {
 	@NotNull
 	@Size(max = 11, message = "핸드폰 번호 형식이 맞지 않습니다.")
 	private String phoneNumber;	
-	@NotNull
-	private String city;
-	@NotNull
-	private String state;
-	@NotNull
-	private String town;
-	
 	private String password;
 	
 	public User toEntity() {
@@ -41,11 +34,6 @@ public class UserCreateRequest {
 				.password(password)
 				.phoneNumber(this.phoneNumber)
 				.role(Role.ROLE_USER)
-				.address(Address.builder()
-						.city(city)
-						.state(state)
-						.town(town)
-						.build())
 				.build();
 	}
 }
