906944f34be754ab24121a8c9eb46edd75e38f2a
gettls <gettls@daum.net>
2022-05-14 19:56:00 +0900

    Feat: cors 진짜 진짜 최종

--- capstone/src/main/java/com/timcook/capstone/common/config/SecurityConfig.java
+++ capstone/src/main/java/com/timcook/capstone/common/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 				.csrf().disable()
-				.cors();
+				.cors().configurationSource(corsConfigurationSource());
 		
 		http
 				.authorizeRequests()
